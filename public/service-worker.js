17285d78134882ff42c680d8f1f9693dd2ae665a
Maria Sori <mariasori.school@gmail.com>
2022-01-19 16:32:09 -0500

    finish service worker set up

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,6 +17,7 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png'
 ];
 
+//install service worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -26,6 +27,7 @@ self.addEventListener('install', function (e) {
     )
 }); 
 
+//activate service worker
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
@@ -40,6 +42,23 @@ self.addEventListener('activate', function (e) {
                     return caches.delete(keyList[i]);
                 }
             }));
+        })
+    )
+}); 
+
+// intercept fetch requests
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
         })
     )
 }); 
